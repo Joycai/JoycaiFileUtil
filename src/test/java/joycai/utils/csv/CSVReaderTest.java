d8bd862a8e3a07cb7d742ad5770171c20ff9e5a6
zhengxucai <85534078@qq.com>
2018-11-15 19:07:02 +0800

    -hotfix

---
+++ src/test/java/joycai/utils/csv/CSVReaderTest.java
@@ -0,0 +1,13 @@
+package joycai.utils.csv;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CSVReaderTest {
+
+    @Test
+    public void newBeanReader() {
+        assert true;
+    }
+}
